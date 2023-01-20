52e4acbdaee7f56ef9bb1d588f66a384967f9353
Taylor Remund <remundtaylor3@gmail.com>
2023-01-20 15:37:48 -0700

    fixed if i === 0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const addToZero = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      if (arr[i] + arr[j] === 0) {
+      if (arr[i] + arr[j] === 0 && i !== j) {
         return true;
       }
     }
